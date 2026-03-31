65b640ce1fb6c879cc097136841fc2f28c06f260
Xing-boop-tech <oengchheangsing@gmail.com>
2026-03-31 13:34:01 +0700

    static search page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './Pages/auth/LoginPage'
 import SignUpPage from './Pages/auth/SignUpPage'
 import HomePage from './Pages/home/HomePage'
+import SearchPage from './Pages/search/SearchPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
+       
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Pages/search/SearchPage.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./SearchPage.css";
+import { FaHome, FaSearch, FaHeart, FaUser } from "react-icons/fa";
+import rock from "../../assets/rock.jpg";
+
+export default function SearchPage() {
+  const genres = [
+    { name: "ROCK", img: rock },
+    { name: "R&B", img: rock },
+    { name: "CLASSIC", img: rock },
+    { name: "HIP-HOP", img: rock },
+    { name: "JAZZ", img: rock },
+    { name: "ROMANTIC", img: rock },
+  ];
+
+  return (
+    <div className="container">
+      {/* HEADER */}
+      <div className="header">
+        {/* PUT YOUR LOGO HERE */}
+        <div className="logo">LOGO</div>
+
+        <input
+          type="text"
+          placeholder="Search for song, artists, albums..."
+          className="search-bar"
+        />
+      </div>
+
+      {/* TITLE */}
+      <h2 className="title">Top Recommend Genres</h2>
+
+      {/* GRID */}
+      <div className="grid">
+        {genres.map((genre, index) => (
+          <div className="card" key={index}>
+            {/* PUT YOUR IMAGE HERE */}
+            <img src={genre.img} alt={genre.name} />
+            <p>{genre.name}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* NAVBAR */}
+      <div className="navbar">
+        <div className="nav-item">
+          <FaHome />
+          <span>Home</span>
+        </div>
+        <div className="nav-item active">
+          <FaSearch />
+          <span>Search</span>
+        </div>
+        <div className="nav-item">
+          <FaHeart />
+          <span>Favorites</span>
+        </div>
+        <div className="nav-item">
+          <FaUser />
+          <span>Account</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
